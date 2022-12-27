92e7b6166ee81e0c285defbe3c461444f027113e
Rafael Meireles <rafameireles2011@gmail.com>
2022-12-27 13:44:38 -0300

    last update

--- section_4/assistant.py
+++ section_4/assistant.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import os
+import sys
 
 from gtts import gTTS
 from playsound import playsound
@@ -9,7 +11,10 @@ def create_audio(audio, message):
     tts = gTTS(message, lang="pt-br")
     tts.save(audio)
     playsound(audio)
-    
+    os.remove(audio)
+
+
+create_audio(audio= os.getcwd() + os.sep + "section_4" + os.sep + "audio" + os.sep + "welcome.mp3", message="Olá, sou Aurora, sua assistente virtual")
     
 def monitoring_audio():
     recon = sr.Recognizer()
@@ -18,15 +23,34 @@ def monitoring_audio():
             print("Diga algo")
             audio = recon.listen(source)
             try:
-                message = recon.recognize_google(audio, language="pt-br").lower()
-                print(f"Você disse {message}")
+                message = recon.recognize_google(audio, language="pt-br")
+                message = message.lower()
+                print(f"Você disse: {message}")
                 create_audio(audio= os.getcwd() + os.sep + "section_4" + os.sep + "audio" + os.sep + "teste.mp3", message=message)
+                execute_commands(message)
                 break
             except sr.UnknownValueError:
                 pass
             except sr.RequestError:
                 pass
         return message
+
+
+def execute_commands(message):
+    audio = os.getcwd() + os.sep + "section_4" + os.sep + "audio" + os.sep + "message.mp3"
+    if 'fechar assistente' in message:
+        sys.exit()
+    elif 'horas' in message:
+        hour = datetime.now().strftime("%H:%M")
+        phrase = f"Agora são {hour}"
+        create_audio(audio=audio, message=phrase)
+    elif 'desligar computador' and 'uma hora' in message:
+        os.system("shutdown -s -t 3600")
+    elif "desligar computador" and 'meia hora' in message:
+        os.system("shutdown -s -t 1800")
+    elif "cancelar desligamento" in message:
+        os.system("shutdown -a")
+        
     
     
 def main():
@@ -34,6 +58,5 @@ def main():
         monitoring_audio()
         
 
-if __name__ == '__main__':
-    main()
+main()
     
